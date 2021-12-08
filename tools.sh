1d2ce10280c4e14f3543db12fea433d414ba9438
IamLunchbox <56757745+IamLunchbox@users.noreply.github.com>
2021-12-09 00:22:20 +0100

    Update tools.sh

--- tools.sh
+++ tools.sh
@@ -1,7 +1,21 @@
 #!/usr/bin/env bash
 set -e -u -o pipefail
 
+get_tools() {
+sudo apt install -y zsh zsh-autosuggestions zsh-syntax-highlighting git
 
-sudo apt install -y curl zsh zsh-autosuggestions zsh-syntax-highlighting 
+chsh -s /bin/zsh
+
+}
+set_prefs() {
+
+cd $HOME
+git clone https://github.com/IamLunchbox/dotfiles .dotfiles
+cd .dotfiles
+./install
+}
+
+get_tools
+set_prefs
 
 exit 0
