1707bb198aa03441385660cecbd2b156d1497a38
IamLunchbox <56757745+IamLunchbox@users.noreply.github.com>
2021-12-08 21:32:45 +0100

    Update deploy_motd.sh

--- deploy_motd.sh
+++ deploy_motd.sh
@@ -1,13 +1,12 @@
 #!/usr/bin/env
-set -u -e -o pipefail
+set -u -o pipefail
 
 if [[ ! -d ./motd ]]; then
   echo "There seems to be no script directory. Quitting."
   exit 1
-fi
-
-if [[ -d "/etc/update-motd.d" ]]; then
-  sudo cp -i ./motd/* /etc/update-motd.d
 else
-    echo "Did not find the update-directory. Quitting"
+  sudo cp -i ./motd/* /etc/update-motd.d/
+  if [[ $? -eq 0 ]]; then
+    echo "Did successfully copy the files to the destination :("
+  fi
 fi
