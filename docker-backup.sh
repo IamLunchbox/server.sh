738ba526ce2a8071593f83f5fe39e508cfb2e941
IamLunchbox <r.grieger@hotmail.com>
2021-12-28 18:14:16 +0100

    Tested the backup routine, logs will be appended to backup.log now

--- docker-backup.sh
+++ docker-backup.sh
@@ -6,47 +6,38 @@ set -u -o pipefail
 
 ## default vars
 help="$0 run|dry-run
-A small helper script to backup a docker-compose directory to an nfs share.
-"
+A small helper script to backup a docker-compose directory to an nfs share."
 scriptname="$0"
-
+timestamp="[$(date +%F-%T]): "
 alias realpath="realpath -e"
 
 prepare() {
-if [[ ! -f "./docker-backup.env" ]]; then
-  echo "You did not provide a necessary environment file. Exiting."
-  exit 1
-else
-  source "./docker-backup.env"
-  starting_point="$(pwd)"
-  backup_dir="backup-$(date +%s)"
-  backup_path="$(realpath "${backup_location}")/${backup_dir}"
-fi
-if [[ -d "${backup_location}" ]] && [[ $(mount -l | grep "${backup_location}") ]]; then
-  sudo -u ${user} rm -rf "${backup_location}"/*
-  sudo -u ${user} mkdir "${backup_path}"
-else
-  echo "Either your backup location does not exist or is not mounted. Exiting."
-  exit 2
-fi
+  echo "${timestamp}Docker Backup start."
+  if [[ ! -f "./docker-backup.env" ]]; then
+    echo "${timestamp}You did not provide a necessary environment file. Exiting."
+    exit 1
+  else
+    source "./docker-backup.env"
+    starting_point="$(pwd)"
+    backup_dir="backup-$(date +%s)"
+    backup_path="$(realpath "${backup_location}")/${backup_dir}"
+  fi
+  if [[ -d "${backup_location}" ]] && [[ $(mount -l | grep "${backup_location}") ]]; then
+    sudo -u ${user} rm -rf "${backup_location}"/*
+    sudo -u ${user} mkdir "${backup_path}"
+  else
+    echo "${timestamp}Either your backup location does not exist or is not mounted. Exiting."
+    exit 2
+  fi
 
-if [[ ! $(id -u) == 0 ]]; then
-  echo "This script is intended for automated usage and needs to be run as root (as of now).
+  if [[ ! $(id -u) == 0 ]]; then
+    echo "${timestamp}This script is intended for automated usage and needs to be run as root (as of now).
 Otherwise this script could not impersonate a given user."
-  exit 3
-
-elif [[ stat ${scriptname} ]]; then
-    exit 4
-
-elif [[ stat "docker-backup.env" ]]; then
-    exit 5
-
-elif [[ ${#services[@]} -lt 1 ]]; then
-  echo "You have given no services to backup."
-  echo "$help"
-  exit 6
-fi
-
+    exit 3
+  elif [[ ${#services[@]} -lt 1 ]]; then
+    echo "${timestamp}You have given no services to backup."
+    exit 6
+  fi
 
 }
 
@@ -54,14 +45,17 @@ docker_backup() {
 for service in ${services[@]}; do
     servicepath="$(realpath "${service_path}")/${service}"
     cd $servicepath
+    echo "${timestamp}Turning off ${service}"
     docker-compose down
     cd ..
+    echo "${timestamp}Backing up ${service}"
     tar -cpz --acls --xattrs -f - "${service}" | sudo -u ${user} tee "${backup_path}"/"${service}".tar.gz 1>/dev/null
 done <<< "${services}"
 
 for service in ${services[@]}; do
   servicepath="$(realpath "${service_path}")/${service}"
   cd $servicepath
+  echo "${timestamp}Starting ${service}"
   docker-compose up -d
 done <<< "${services}"
 }
@@ -69,11 +63,12 @@ done <<< "${services}"
 
 additional_backups() {
 if [[ ${#additional_dirs[@]} -gt 0 ]]; then
-for directory in ${additional_dirs[@]}; do
-  cd "${directory}"
-  cd ..
-  tar -cpz --acls --xattrs -f - "$(realpath --relative-to="$(pwd)" "$directory")" | sudo -u ${user} tee "${backup_path}"/"$(realpath --relative-to="$(pwd)" "${directory}")".tar.gz 1>/dev/null
-done <<< "${additional_dirs}"
+  for directory in ${additional_dirs[@]}; do
+    cd "${directory}"
+    cd ..
+    echo "${timestamp}Backing up $directory"
+    tar -cpz --acls --xattrs -f - "$(realpath --relative-to="$(pwd)" "$directory")" | sudo -u ${user} tee "${backup_path}"/"$(realpath --relative-to="$(pwd)" "${directory}")".tar.gz 1>/dev/null
+  done <<< "${additional_dirs}"
 fi
 }
 
@@ -81,11 +76,15 @@ fi
 if [[ $# = 1 ]]; then
   case $1 in
     "run")
+      exec 3>&1 4>&2
+      trap 'exec 2>&4 1>&3' 0 1 2 3
+      exec 1>>docker-backup.log 2>&1
+      ;;
+    "quiet")
       true
       ;;
     *)
-      echo "I could not understand you. Quitting."
-      echo "$help"
+      echo "${timestamp}I could not understand you. Quitting."
       exit 1
       ;;
   esac
@@ -93,10 +92,6 @@ else
   echo "$help"
   exit 0
 fi
-
-exec 3>&1 4>&2
-trap 'exec 2>&4 1>&3' 0 1 2 3
-exec 1>>docker-backup.log 2>&1
 
 prepare
 docker_backup
